d7533a078aef5878889929275b83d516cb498914
Diogo Tavares <32261806+DTavarez@users.noreply.github.com>
2022-01-06 02:56:35 +0000

    display weather search results
    created new component that will receive weather data and display it
    added "city not foun" in case of non existent city name

--- src/pages/locationWeather.js
+++ src/pages/locationWeather.js
@@ -1,5 +1,6 @@
 import { useEffect, useState } from 'react';
 import NavBar from '../components/navbar';
+import WeatherResume from '../components/weatherResume';
 import { useSearchParams} from 'react-router-dom';
 import {config} from '../config/config'
 import axios from 'axios';
@@ -13,21 +14,41 @@ function LocationWeather() {
 
   async function getWeatherInfo(name){
     const url = "https://api.openweathermap.org/data/2.5/weather?appid=" + config.weatherApiKey + "&q=" + String(name);
-    console.log(url)
-    const res = await axios.get(url);
 
-    if(res.status === 200){
-      setSearchError(false);
-      console.log(res.data);
-      setWeatherInfo(res.data);
-    }else{
-      setSearchError(true);
+    try {
+      const res = await axios.get(url);
+      console.log(res);
+      if(res.status === 200){
+        setSearchError(false);
+        console.log(res.data);
+        setWeatherInfo(res.data);
+      }else{
+        console.log("eroooo")
+        setSearchError(true);
+      }
+
+    } catch(error) {
+    
+        setSearchError(true);
+        if (error.response) {
+          // Request made and server responded
+          console.log(error.response.data);
+          console.log(error.response.status);
+          console.log(error.response.headers);
+        } else if (error.request) {
+          // The request was made but no response was received
+          console.log(error.request);
+        } else {
+          // Something happened in setting up the request that triggered an Error
+          console.log('Error', error.message);
+        }
     }
   }
 
   useEffect(() => {
     const name = searchParams.get("city");
     if(name){
+      setWeatherInfo(null);
       setLoading(true);
       getWeatherInfo(name);
     }else{
@@ -39,11 +60,12 @@ function LocationWeather() {
   return (
     <div className="App">
       <NavBar></NavBar>
-      <div>
-        {weatherInfo &&
-          <div>
-          {JSON.stringify(weatherInfo)}
-          </div>
+      <div className='body-container'>
+        {weatherInfo && !searchError &&
+          <WeatherResume weatherInfo={weatherInfo}/>
+        }
+        {searchError &&
+          <div>city not found.</div>
         }
       </div>
     </div>
